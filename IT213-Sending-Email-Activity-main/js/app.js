82e2c33d766520fc421a6a966fa0301e9f0c7e61
balbalosa80gina03 <balbalosa80gina03@gmail.com>
2021-12-06 13:07:32 +0800

    new changes
    Signed-off-by: balbalosa80gina03 <balbalosa80gina03@gmail.com>

--- IT213-Sending-Email-Activity-main/js/app.js
+++ IT213-Sending-Email-Activity-main/js/app.js
@@ -8,17 +8,6 @@ const sendBtn = document.getElementById('sendBtn'),
 
 
 
-    const sendBtn = document.getElementById('sendBtn'),
-    email = document.getElementById('email'),
-    subject = document.getElementById('subject'),
-    message = document.getElementById('message'),
-    resetBtn = document.getElementById('resetBtn'),
-    sendEmailForm = document.getElementById('email-form');
-
-
-
-
-
 eventListeners();
 
 function eventListeners() {
