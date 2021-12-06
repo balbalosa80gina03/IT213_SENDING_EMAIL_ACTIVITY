89153523cdfa442e33e96071f0000ba7a8d5a022
balbalosa80gina03 <95342536+balbalosa80gina03@users.noreply.github.com>
2021-12-06 10:20:54 +0800

    Add files via upload

---
+++ IT213-Sending-Email-Activity-main/js/app.js
@@ -0,0 +1,10 @@
+const sendBtn = document.getElementById('sendBtn'),
+    email = document.getElementById('email'),
+    subject = document.getElementById('subject'),
+    message = document.getElementById('message'),
+    resetBtn = document.getElementById('resetBtn'),
+    sendEmailForm = document.getElementById('email-form');
+
+
+
+
